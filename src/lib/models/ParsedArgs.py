ad46f6bd6c0de0ec3e1105fd40255a4e35a328f9
Romain Mularczyk <romain.mularczyk@gmail.com>
2024-06-20 12:42:30 +0200

    [MOD] allow_arbitrary_models

--- src/lib/models/ParsedArgs.py
+++ src/lib/models/ParsedArgs.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
+from lib.models.Policies import Policies
 
 
 class ParsedArgs(BaseModel):
     training: bool
     episodes: int
     gamma: float
-    #policy: Policy
+    policy: Policies
+
+    class Config:
+        arbitrary_types_allowed = True
