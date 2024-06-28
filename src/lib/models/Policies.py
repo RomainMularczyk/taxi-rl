6aef1bbc36093d1862aa24e3416e7ed74b40cfd9
Romain Mularczyk <romain.mularczyk@gmail.com>
2024-06-28 10:09:30 +0200

    [MOD] random policies name

--- src/lib/models/Policies.py
+++ src/lib/models/Policies.py
@@ -2,9 +2,11 @@ from typing import Union
 from lib.policies.LegalSamplePolicy import LegalSamplePolicy
 from lib.policies.RandomSamplePolicy import RandomSamplePolicy
 from lib.policies.MonteCarloPolicy import MonteCarloPolicy
+from lib.policies.MaxPolicy import MaxPolicy
 
 
 Policies = Union[
+    MaxPolicy,
     LegalSamplePolicy,
     RandomSamplePolicy,
     MonteCarloPolicy
