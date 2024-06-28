77625a1804002d6543780d4bde1a0bcdd9ce3079
Romain Mularczyk <romain.mularczyk@gmail.com>
2024-06-28 10:10:03 +0200

    [MOD] env passed as arg to policy

--- src/lib/policies/RandomSamplePolicy.py
+++ src/lib/policies/RandomSamplePolicy.py
@@ -1,5 +1,8 @@
-from gymnasium.wrappers.time_limit import TimeLimit as GymnasiumGameEnvironment
+import logging
+from pydantic import TypeAdapter, ValidationError
+from lib.environment.environment import GameEnvironment
 from lib.models.Action import Action
+from lib.models.EnvironmentInfo import EnvironmentInfo
 
 
 class RandomSamplePolicy:
@@ -12,13 +15,28 @@ class RandomSamplePolicy:
     ----------
     p: float
         The probability of a given action to be taken.
+    env: GameEnvironment
+        The game environment.
+    reward: float
+        The reward collected for taking a given step.
+    state: int
+        The seed representing a given state of the game environment.
     """
-    p: float = 1/6
 
-    @staticmethod
+    def __init__(
+        self,
+        env: GameEnvironment,
+        p: float = 1/6
+    ):
+        self.p = p
+        self.env = env
+        self.reward = 0.0
+
     def next_action(
-        env: GymnasiumGameEnvironment,
-        human_readable: bool = False
+        self,
+        human_readable: bool = False,
+        render: bool = True,
+        display_actions: bool = False
     ):
         """
         Choose the optimal next action.
@@ -29,13 +47,38 @@ class RandomSamplePolicy:
             The game environement.
         human_readable: bool, default=False
             Output the action chosen in a human readable format.
+        render: bool, default=True
+            Defines if the step should be displayed.
+        display_actions: bool, default=False
+            Defines if the available actions at a given step should be
+            displayed.
 
         Returns
         -------
         int | str
             The action chosen.
         """
-        action = env.action_space.sample()
+        action = self.env.env.action_space.sample()
+
+        observation, reward, terminated, truncated, info = self.env.env.step(
+            action
+        )
+        self.state = observation[0]
+
+        if render:
+            logging.info(self.env.env.render())
+        if display_actions:
+            try:
+                TypeAdapter(EnvironmentInfo).validate_python(info)
+                logging.info(Action.available_actions(info))  # type: ignore
+            except ValidationError:
+                logging.error(
+                    "The environment information does"
+                    " not have the expected format."
+                )
+
+        self.reward += float(reward)
+
         if human_readable:
             return Action.to_human_readable(action)
         return action
