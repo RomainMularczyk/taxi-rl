407e44de3d1002d79f0d89b1b421f778c59baca5
Romain Mularczyk <romain.mularczyk@gmail.com>
2024-06-28 10:08:50 +0200

    [ADD] q learning tests

---
+++ src/tests/formulas/q_learning/test_expected_value.py
@@ -0,0 +1,19 @@
+from lib.formulas.q_learning import QLearning
+from lib.policies.RandomSamplePolicy import RandomSamplePolicy
+
+
+def test_dice_expected_value():
+    policy = RandomSamplePolicy()
+    q_learning = QLearning(
+        initial_seed=0,
+        cutoff_score=0,
+        observation_space=5,
+        action_space=5,
+        policy=policy
+    )
+    result = q_learning.expected_value(6)
+    expected = 3.5
+    assert result == expected
+
+def test_one_expected_value():
+    pass
