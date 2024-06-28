e3f68816a29212376ba9a17f82ed6f8140733316
Romain Mularczyk <romain.mularczyk@gmail.com>
2024-06-28 21:07:53 +0200

    [ADD] q learning unit tests

--- src/tests/formulas/q_learning/test_q_function_update.py
+++ src/tests/formulas/q_learning/test_q_function_update.py
@@ -1,13 +1,15 @@
+import gymnasium as gym
 from lib.formulas.q_learning import QLearning
 from lib.policies.RandomSamplePolicy import RandomSamplePolicy
 
 
 def test_q_learning_q_function_update():
+    env = gym.make("Taxi-v3")
     q_learning = QLearning(
         initial_seed=0,
         cutoff_score=0,
         observation_space=5,
         action_space=4,
-        policy=RandomSamplePolicy(),
+        policy=RandomSamplePolicy(env=env),  # type: ignore
         gamma=1.0
     )
