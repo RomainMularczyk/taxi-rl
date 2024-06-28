9606781475e17456b16a9b927846ec060ca5285e
Romain Mularczyk <romain.mularczyk@gmail.com>
2024-06-28 21:06:14 +0200

    [ADD] location method

--- src/lib/models/Passenger.py
+++ src/lib/models/Passenger.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+from typing import Tuple
 from enum import Enum
 
 
@@ -12,7 +14,44 @@ class Passenger(Enum):
     TAXI = 4
 
     @staticmethod
-    def to_human_readable(passenger: int) -> str:
+    def location(
+        state: int,
+        absolute: bool = False
+    ) -> Passenger | Tuple[int, int] | None:
+        """
+        Return the location of the passenger given the current
+        state of the game environment.
+
+        Parameters
+        ----------
+        state: int
+            The state of the game environment.
+        absolute: bool, default=False
+            Return the absolute coordinate of the passenger location.
+
+        Returns
+        -------
+        Passenger | Tuple[int, int] | None
+            The passenger location. If in absolute mode, returns
+            the coordinates in which the passenger is located. It will
+            return 'None' if the passenger is in the taxi.
+        """
+        passenger_location = Passenger((state // 4) % 5)
+        if absolute:
+            if passenger_location == Passenger.BLUE:
+                return (4, 3)
+            elif passenger_location == Passenger.GREEN:
+                return (0, 4)
+            elif passenger_location == Passenger.RED:
+                return (0, 0)
+            elif passenger_location == Passenger.YELLOW:
+                return (4, 0)
+            else:
+                return None
+        return passenger_location
+
+    @staticmethod
+    def to_human_readable(passenger: int) -> Passenger:
         """
         Convert the passenger location to a human readable format.
 
@@ -23,10 +62,10 @@ class Passenger(Enum):
 
         Returns
         -------
-        str
+        Passenger
             The passenger location in a human readable format.
         """
-        return Passenger(passenger).name
+        return Passenger(passenger)
 
     @staticmethod
     def from_human_readable(passenger: str) -> int:
