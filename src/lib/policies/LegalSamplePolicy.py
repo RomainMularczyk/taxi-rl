8771a8ca6ccc25b1dcb5d4ce473736ab66b6ad02
Romain Mularczyk <romain.mularczyk@gmail.com>
2024-07-04 11:31:01 +0200

    [ADD] possible actions + next action

--- src/lib/policies/LegalSamplePolicy.py
+++ src/lib/policies/LegalSamplePolicy.py
@@ -1,6 +1,8 @@
+import random
 import numpy as np
+from typing import List
 from gymnasium.wrappers.time_limit import TimeLimit as GymnasiumGameEnvironment
-from lib.models.Action import Action, ActionProbabilities
+from lib.models.Action import Action, ActionProbabilities, ActionWithReward
 from lib.policies.Policy import Policy
 
 
@@ -13,8 +15,8 @@ class LegalSamplePolicy(Policy):
 
     Attributes
     ----------
-    p: float
-        The probability of a given action to be taken.
+    type: Policy.Type
+        Defines if the policy is deterministic or probabilistic.
     """
 
     def __init__(
@@ -23,44 +25,84 @@ class LegalSamplePolicy(Policy):
         seed: int | None = None
     ) -> None:
         super().__init__(env=env, seed=seed)
-        self.reward = 0.0
+        self.type = Policy.Type.PROBABILISTIC
 
     def actions_probability(self) -> ActionProbabilities:
-        action_names = [
-            action.name for action, legal in zip(
-                list(Action), self.env.info.action_mask
-            ) if legal == 1
-        ]
+        """
+        Returns the probability associated with each action.
+
+        Returns
+        -------
+        ActionProbabilities
+            The probability associated with each legal action.
+        """
+        legal_actions_names = [action.name for action in self.legal_actions()]
         num_legal_actions = len(
             self.env.info.action_mask[self.env.info.action_mask == 1]
         )
         action_prob = {
             action: prob for action, prob in zip(
-                action_names, list(
+                legal_actions_names, list(
                     np.full(num_legal_actions, 1/num_legal_actions)
                 )
             )
         }
-        return action_prob  # type: ignore
+        return ActionProbabilities(**action_prob)
 
-    def next_action(
-        self,
-        human_readable: bool = False
-    ):
+    def legal_actions(self) -> List[Action]:
         """
-        Choose the optimal next action.
-
-        Parameters
-        ----------
-        human_readable: bool, default=False
-            Output the action chosen in a human readable format.
+        Returns the legal actions at a given state.
 
         Returns
         -------
-        int | str
+        List[Action]
+            A list of legal actions.
+        """
+        legal_actions = [
+            action for action, legal in zip(
+                list(Action), self.env.info.action_mask
+            ) if legal == 1
+        ]
+        return legal_actions
+
+    def possible_actions(self) -> List[Action]:
+        """
+        Return all the next available actions following the policy.
+
+        Returns
+        -------
+        List[Action]
+            A list of actions.
+        """
+        return self.legal_actions()
+
+    def next_action(  # type: ignore
+        self,
+        mask: np.ndarray | None,
+        probability: float | None = None
+    ) -> ActionWithReward:
+        """
+        Choose the optimal next action.
+
+        Returns
+        -------
+        Action
             The action chosen.
         """
-        action = self.env.env.action_space.sample()
-        if human_readable:
-            return Action.to_human_readable(action)
-        return action
+        if mask is not None:
+            action = self.env.env.action_space.sample(mask)
+            new_state, reward, _, _, _ = self.env.env.step(action)
+            self.env.state = new_state
+            return ActionWithReward(
+                action=Action(action),
+                reward=float(reward),
+                probability=probability
+            )
+        else:
+            action = random.choice(self.legal_actions())
+            action = self.take_action(action)
+            return ActionWithReward(
+                action=action.action,
+                reward=action.reward,
+                probability=float(1/len(self.legal_actions()))
+            )
